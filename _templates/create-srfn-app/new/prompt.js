8ba99e12743b235c0eed83b650095cc55989036c
Robert Kempton <r@snow87.com>
2021-09-30 22:47:31 -0600

    templatized everything, app starts up with all options configured, but not functioning correctly

--- _templates/create-srfn-app/new/prompt.js
+++ _templates/create-srfn-app/new/prompt.js
@@ -1,1 +1,3 @@
+// this file has to exist for prompt variables to work.
+// Prompt is manually done in index.js to allow conditional prompts
 module.exports = []
--- index.js
+++ index.js
@@ -1,48 +1,35 @@
 #!/usr/bin/env node
 const path = require( 'path' );
+const os = require('os');
+const fs = require('fs');
 const { runner } = require( 'hygen' );
 const Logger = require( 'hygen/lib/logger' );
 const defaultTemplates = path.resolve( __dirname, 'templates' );
-const { exec } = require( 'child_process' )
+const { spawnSync } = require( 'child_process' )
 const { prompt } = require( 'enquirer' )
 
 async function run() {
-
 
     const config = await prompt( [
         {
             type: 'input',
             name: 'appName',
-            message: "Enter your new app's name:"
+            message: "Enter your app's name:"
         },
         {
             type: 'confirm',
             default: false,
             name: 'userApiEnabled',
-            message: "Enable UserApi?"
+            message: "Enable User Api?"
         },
         {
             type: 'confirm',
             name: 'authEnabled',
             default: false,
-            message: "Enable auth?"
+            message: "Enable JWT auth?"
         },
 
     ] )
-
-    if( config.authEnabled ) {
-        Object.assign( config, await prompt( [
-                {
-                    type: 'confirm',
-                    name: 'googleAuthEnabled',
-                    default: false,
-                    message: "Enable Google Auth"
-                }
-            ] )
-        )
-    } else {
-        config.googleAuthEnabled = false
-    }
 
     if(config.userApiEnabled) {
         Object.assign(config, await prompt([
@@ -57,6 +44,20 @@ async function run() {
         config.rolesEnabled = false
     }
 
+    if( config.authEnabled ) {
+        Object.assign( config, await prompt( [
+                {
+                    type: 'confirm',
+                    name: 'googleAuthEnabled',
+                    default: false,
+                    message: "Enable Google Auth?"
+                }
+            ] )
+        )
+    } else {
+        config.googleAuthEnabled = false
+    }
+
     await runner( [
         'create-srfn-app',
         'new'
@@ -67,8 +68,25 @@ async function run() {
         logger: new Logger( console.log.bind( console ) ),
         templates: defaultTemplates
     })
-    exec( 'npm i', { cwd: path.join( process.cwd(), config.appName ) } )
+    await spawnSync( 'npm',['i'], { stdio: 'inherit', cwd: path.join( process.cwd(), config.appName ) } )
     return config
 }
 
-run().then(config=>console.log( 'App initialized!\nTry `cd '+config.appName+' && npm run start` to start your app.' ))
+run().then(config=>{
+    const home = os.homedir()
+    let appEnv = path.join(home, `.${config.appName}.env`);
+    if(fs.existsSync(appEnv)){
+        console.log(`${appEnv} already exists. You may need to update it to work for this new application.`)
+    } else {
+        fs.writeFileSync(appEnv, `#do not check this file in to source control!!\nSRFN_APP_NAME=${config.appName}\n`)
+        if(config.authEnabled){
+            console.warn(`With auth enabled, you should update JWT_SECRET in ${appEnv}.`)
+            fs.appendFileSync(appEnv, 'JWT_SECRET=change_this_value\n')
+        }
+        if(config.googleAuthEnabled){
+            console.error(`With google auth enabled, you need to update GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET in ${appEnv} before your application will start.`)
+            fs.appendFileSync(appEnv, 'GOOGLE_CLIENT_ID=123456\nGOOGLE_CLIENT_SECRET=654321\n')
+        }
+    }
+    console.log( 'App initialized!\nTry `cd '+config.appName+' && docker-compose up -d && npm run start` to start your app.' )
+})
